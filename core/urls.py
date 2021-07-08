dd4f74122d42233cf2befd86c03bd26a311e3c44
Benja <b.pinom01@gmail.com>
2021-07-08 01:38:28 -0400

    artistas

--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,11 @@ from .views import inicio,formularioArtista,form_mod,form_del,home,artista1,arti
 
 
 urlpatterns = [
-    path('',inicio,name="inicio"),
+    path('',home,name="home"),
     path('formularioArtista',formularioArtista,name="formulario"),
     path('form-mod/<id>',form_mod,name="form_mod"),
     path('form-del/<id>',form_del,name="form_del"),
-    path('home',home,name="home"),
+    path('inicio',inicio,name="inicio"),
     path('artista1',artista1,name="artista1"),
     path('artista2',artista2,name="artista2"),
     path('artista3',artista3,name="artista3"),
